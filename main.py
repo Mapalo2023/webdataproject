cace8505f466adf4ae64d9c44c2551635c09db57
MAPALO LUKASHI <mlukashi@mail.yu.edu>
2023-11-28 17:02:59 -0500

    creating main.py file

---
+++ main.py
@@ -0,0 +1,8 @@
+# main.py in my_webdatapack package
+
+def main():
+    # instantiating my scraper and analysis classes and running them.
+    print("Web Data Analysis Package is running!")
+
+if __name__ == "__main__":
+    main()
